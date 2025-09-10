dcc6ce0370e42a3d8fc3f1d127d9a48cb80e32de
DREAMMUSICRECORDS <yeribthvivas@gmail.com>
2025-09-10 10:22:59 -0500

    cambios
    se anexa carpera java script

---
+++ js/script.js
@@ -0,0 +1,25 @@
+alert("Bienvenido a royalnieto");
+var entradas = 30;
+var contador = 0;
+var estado = "s"
+
+var entradas = parseInt (prompt ("escribe el numero de entradas"));
+ if (entradas <=30 && entradas >= 1){
+    alert ("entradas disponibles");
+    
+
+
+    }
+    else if  (entradas2 >=31 && entradas >= 31 ){
+     alert ("no hay suficientes entradas");
+    }
+
+
+  const numeroentradas = 30;
+  const numeroqueseingresa = prompt("ingrasa otra vez el numero de entradas que quieres:");
+  resultado = numeroentradas - numeroqueseingresa;
+  Number.isNaN(numeroentradas - numeroqueseingresa);
+
+  alert( resultado = numeroentradas-numeroqueseingresa);
+
+  var entradas = parseInt (prompt ("escribe el numero de entradas"));
